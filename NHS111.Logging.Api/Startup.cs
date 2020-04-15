b235ccc2a1eda6ae7b2a4ade7731c61d2e0ba4bc
Wendy Lee <wendy.lee2@nhs.net>
2020-04-15 14:26:56 +0100

    ON-1762: Remove redundant using and add slash before api on web.config

--- NHS111.Logging.Api/Startup.cs
+++ NHS111.Logging.Api/Startup.cs
@@ -1,8 +1,6 @@
-﻿using System.IO;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
